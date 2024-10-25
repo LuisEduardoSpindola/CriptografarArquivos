00eecfcc921914bab035e71fbf6d9d6d8363a8b2
LuisEduardo <luiseduardospindola@gmail.com>
2024-10-24 21:49:04 -0300

    Adicionar arquivos de projeto.

---
+++ CriptografarArquivos/Controller/FileMagicProcessController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace CriptografarArquivos.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileMagicProcessController : ControllerBase
+    {
+        private readonly byte[] key = Encoding.UTF8.GetBytes("ongapo0563049fdakf");
+        private readonly byte[] _iv = Encoding.UTF8.GetBytes("");
+
+    }
+}
